34b328e6bacdb4b9850f74cd18895675877428a3
Harnoor Lal <harlal902@gmail.com>
2025-07-23 13:54:32 -0700

    feat: Implement Card and Deck classes

---
+++ poker/deck.py
@@ -0,0 +1,16 @@
+import random
+from .card import Card
+
+class Deck:
+    def __init__(self):
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+        self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self):
+        if len(self.cards) > 0:
+            return self.cards.pop()
+        return None
